97cd2f00b4b31c5572398c4962e10c33dfa6a1ef
Mayuresh Agashe <75118658+mayureshagashe2105@users.noreply.github.com>
2023-11-01 23:22:31 +0530

    Update router.py

--- server/router.py
+++ server/router.py
@@ -21,6 +21,6 @@ def send_email(bgtasks: BackgroundTasks, details: EmailSchema):
   "from": "onboarding@resend.dev",
   "to": details.recipients[0],
   "subject": "Techdocs",
-  "HTML": f"<a href="{details.template_kwargs['verify_link']}">link</a><p>Congrats on sending your <strong>first email</strong>!</p>"
+  "HTML": f'''<a href="{details.template_kwargs['verify_link']}">link</a><p>Congrats on sending your <strong>first email</strong>!</p>'''
 })
 
