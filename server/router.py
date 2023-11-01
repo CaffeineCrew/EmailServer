b0c9504018ecefa665cb12565210c6cf9d4f577c
Mayuresh Agashe <75118658+mayureshagashe2105@users.noreply.github.com>
2023-11-01 22:56:58 +0530

    Update router.py

--- server/router.py
+++ server/router.py
@@ -16,4 +16,4 @@ app.add_middleware(
 
 @app.post("/api/send",tags=["mail"])
 async def send_email(bgtasks: BackgroundTasks, details: EmailSchema):
-    await ops_send_email(details)
+    await ops_send_email(bgtasks, details)
